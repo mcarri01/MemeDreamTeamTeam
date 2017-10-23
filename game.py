e2528d5b8f0dc35d4d9b68748d19182b7065156c
David Stern <davidestern14@gmail.com>
2017-10-23 16:14:26 -0400

    Matt is gonna owe us drinks after this one I'M READY

--- game.py
+++ game.py
@@ -1,9 +1,45 @@
 import sys
 import os
+import threading
+# for networking
+#import pyro
+
+board = []
+fish = []
+shark = []
+
+def readShark(filename):
+        with open(filename) as f:
+                for line in f:
+                        shark.append(line.split('\n')[0])
+
+def readFishy(filename):
+	with open(filename) as f:
+		for line in f:
+			fish.append(line.split('\n')[0])
+	
+def drawBoard(height, width):
+	for j in range(height):
+		string = '+'
+		for i in range(width):
+			if j != 0 and j != (height - 1):
+				string += ' '
+			else:
+				string += '-'
+		string += '+'
+		board.append(string)
 
 def main(argv):
-	print argv
-
-
+	drawBoard(30, 200)
+	readShark("shark.txt")
+	readFishy("fish.txt")
+	for i in shark:
+		print i
+	for i in fish:
+		print i
+	for i in board:
+		print i
+	
+	
 if __name__ == "__main__":
 	main(sys.argv)
